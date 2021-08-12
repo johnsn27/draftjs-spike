19d5c9bc9e740dff855c04b25bcc3211e131b5b6
johnsn27 <nathan.johnson@bbc.co.uk>
2021-08-12 12:19:21 +0100

    Add introduction text

--- draftjs-spike-app/src/App.js
+++ draftjs-spike-app/src/App.js
@@ -2,11 +2,18 @@ import React, { Component } from 'react';
 import './App.css';
 import RichTextEditor from './components/Editor'
 
+const Introduction = () => (
+  <section>
+    <h1>This is a demo of a new text editor we would like feedback on</h1>
+    <p>This demo application contains a new text editor. The editor has rich text editing cababilities with the use of the bold and italic buttons</p>
+  </section>
+)
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Introduction />
         <RichTextEditor />
       </div>
     );
